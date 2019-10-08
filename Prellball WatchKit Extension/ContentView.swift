770ab71573c21c1d3cfbde0962b37bc99712bac1
Jeremy Boy <github@jboy.eu>
2019-10-08 08:54:41 +0200

    Add CompetitorView

--- Prellball WatchKit Extension/ContentView.swift
+++ Prellball WatchKit Extension/ContentView.swift
@@ -9,13 +9,44 @@
 import SwiftUI
 
 struct ContentView: View {
+
+    @State var viewModel: PrellViewModel = PrellViewModel()
+
     var body: some View {
-        Text("Hello World")
+        ScrollView {
+            HStack {
+                competitorView(for: .home)
+                competitorView(for: .visitor)
+            }
+            Button("Reset") { self.viewModel.reset() }
+            Button("Vorlesen") { self.viewModel.read() }
+        }
+    }
+
+    private func competitorView(for c: Competitor) -> CompetitorView {
+        return CompetitorView(text: viewModel.value(for: c),
+                              increment: { self.viewModel.increment(for: c)},
+                              decrement: { self.viewModel.decrement(for: c)})
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct CompetitorView: View {
+
+    let text: String
+    let increment: () -> Void
+    let decrement: () -> Void
+
+    var body: some View {
+        return VStack {
+            Text(text)
+            Button("+", action: increment)
+            Button("-", action: decrement)
+        }
     }
 }
